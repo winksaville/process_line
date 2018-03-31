385d579b37ea92198b046d2ee1490cb5819fcb42
Wink Saville <wink@saville.com>
2018-03-31 09:49:56 -0700

    process_line is "working"
    It passes all of the tests but I still have a question on why a loop
    can be the same as "g" flag in substitute commands.

--- process_line.sh
+++ process_line.sh
@@ -6,21 +6,23 @@
 # my last name, "saville" if it isn't already present.
 
 process_line () {
-	# This replaces "wink" with "wink saville" unconditionally
-	# Working OK except "wink wink" should work and it doesn'!
-	echo $(sed -E 's/(\W+|^)(wink)(\W+|$)/\1\2 saville\3/g' <<<"$@")
+	# Simplest solution I could figure out
+	echo $(sed -E 's/\bwink\b|\bwink saville\b/wink saville/g' <<<"$@")
+
+	# Also using 'vim' style with \< and \> for beginning/end word boundaries
+	#echo $(sed -E 's/\<wink\>|\<wink saville\>/wink saville/g' <<<"$@")
+
+	# From [1] \b is an anchor like BOL ^ and EOL $ and is zero length
+	# so it captures nothing and this makes no difference:
+	#echo $(sed -E 's/(\b)wink(\b)|(\b)wink saville(\b)/\1\1wink saville\2\2/g' <<<"$@")
 
 	# Attempt to simulate "g" with loops, isn't working why?
-	# $!btop is from [1] and says branch to top if not EOL.
-	#echo $(sed -E '{ : top ; s/(\W+|^)(wink)(\W+|$)/\1\2 saville\3/ ; $!btop } ' <<<"$@")
+	# $!btop is from [2] and says branch to top if not EOL.
+	#echo $(sed -E '{ : top ; s/\bwink\b/wink saville/ ; $!btop } ' <<<"$@")
 
-	# Here's an attempt that conditionally handles
-	# "wink" or "wink saville" once, but isn't looping
-	# properly.
-	#echo $(sed -E '{ : top ; s/(\W+|^)(wink)(\W+)(saville)(\W+|$)/\1\2\3\4\5/ ; t btm ; s/(\W+|^)(wink)(\W+|$)/\1\2 saville\3/ ; : btm ; $!btop }' <<<"$@")
-
-	# Footnotes
-	# [1]: https://stackoverflow.com/questions/1251999/how-can-i-replace-a-newline-n-using-sed
+# Footnotes
+# [1]: https://www.regular-expressions.info/wordboundaries.html
+# [2]: https://stackoverflow.com/questions/1251999/how-can-i-replace-a-newline-n-using-sed
 }
 
 test_expect_success () {
@@ -45,6 +47,7 @@ test_expect_success "yowink winks" "yowink winks"
 test_expect_success "yowink wink" "yowink wink saville"
 test_expect_success "wink winkyo" "wink saville winkyo"
 test_expect_success "wink wink" "wink saville wink saville"
+test_expect_success "wink   wink" "wink saville wink saville"
 test_expect_success "wink saville" "wink saville"
 test_expect_success "hi wink, yo wink" "hi wink saville, yo wink saville"
 test_expect_success "wink saville wink" "wink saville wink saville"
